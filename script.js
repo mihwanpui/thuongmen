8ab46c024f88f87f4cdf2c6e89f5cbd11d8772dd
mihwanpui <mquanbui262002@gmail.com>
2024-06-07 01:55:09 +0700

    thuongmen update

--- script.js
+++ script.js
@@ -1,7 +1,63 @@
-document.getElementById('accept').onclick = function() {
-    alert('Cảm ơn bé iu nhiều lắm! Anh yêu em! ❤️');
-};
-document.getElementById('reject').onclick = function() {
-    alert('Anh sẽ cố gắng hơn để làm em vui ! 😢');
-    this.style.display = 'none';  // Ẩn nút 'Không bao giờ'
-};
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  }
+});
+
+function handleYesClick() {
+  titleElement.innerHTML = "Anh iu bé ,Anh hứa hongg làm bé buồn nữa đouuu :3";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "Không Bao Giờ",
+    "Anh bicc lỗi rồi ạa",
+    "Mong bé tha lỗi choo anhh :((",
+    "Anhh saii rồi , anhh đáng trách ạ",
+    "Bé đừng giận anhh nữa nhoo",
+    "Anhhh iu bé nhắm nhunnn đóoooo",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
