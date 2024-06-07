5eaea84e6409e8ab63b3ee39f9276e4ea1e22de5
mihwanpui <129419778+mihwanpui@users.noreply.github.com>
2024-06-07 16:16:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const titleElement = document.querySelector(".title");
 const buttonsContainer = document.querySelector(".buttons");
 const yesButton = document.querySelector(".btn--yes");
 const noButton = document.querySelector(".btn--no");
-const catImg = document.querySelector(".cat-images");
+const catImg = document.querySelector(".cat-img");
 
 const MAX_IMAGES = 5;
 
